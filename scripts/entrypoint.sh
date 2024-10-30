fbc7d42db535a49b9135468aa5e99178ec080ad9
An <ankhanh47837.hoctot@gmail.com>
2024-10-31 02:10:50 +0700

    Update docker-compose.xml #8

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -6,8 +6,8 @@
 # Wait for SQL Server to start
 sleep 30s
 
-# Run initialization script
-/opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P "$MSSQL_SA_PASSWORD" -i /docker-entrypoint-initdb.d/init.sql
+# Run initialization script with TrustServerCertificate=yes
+/opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P "$MSSQL_SA_PASSWORD" -i /docker-entrypoint-initdb.d/init.sql -C -N -t 30 -b -e
 
 # Keep container running
 wait $!
