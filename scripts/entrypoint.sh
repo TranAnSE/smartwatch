bc7b918bc81c271190590cf8e23d2270f198cb07
An <ankhanh47837.hoctot@gmail.com>
2024-10-31 03:31:31 +0700

    test #2

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
+set -e
 
 # Start SQL Server
 /opt/mssql/bin/sqlservr & 
 
 # Wait for SQL Server to start
-sleep 30s
+echo "Waiting for SQL Server to start..."
+sleep 30
 
-# Run initialization script with TrustServerCertificate=yes
-/opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P "$MSSQL_SA_PASSWORD" -i /docker-entrypoint-initdb.d/init.sql -C -N -t 30 -b -e
+# Run initialization script
+echo "Running initialization script..."
+/opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P "$MSSQL_SA_PASSWORD" -i /docker-entrypoint-initdb.d/init.sql
 
 # Keep container running
 wait $!
