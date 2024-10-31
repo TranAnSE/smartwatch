bb44e5b337b0d72cd12ef8097f251e535bcfcb1e
An <ankhanh47837.hoctot@gmail.com>
2024-10-31 23:51:48 +0700

    Sửa #5

--- scripts/init-db.sh
+++ scripts/init-db.sh
@@ -8,8 +8,8 @@ set -e
 echo "Waiting for SQL Server to start..."
 sleep 30s
 
-# Run the initialization script with SSL trust
-/opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P "$MSSQL_SA_PASSWORD" -d master -i /usr/src/app/init.sql -C -N
+# Initialize the database
+/opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P "$SA_PASSWORD" -i /usr/src/app/init.sql
 
 # Keep container running
 tail -f /dev/null
